dcb6b8d754153c1e867fd1a5518d4c057a93046b
Kushagra Agrawal <agrawalkushagra29@gmail.com>
2024-01-03 22:08:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className='flex justify-center items-center text-4xl text-red-600'>Welcome to our website!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
